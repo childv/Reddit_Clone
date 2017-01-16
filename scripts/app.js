310cbdd6d315181370e414e4313cd2097ded7cea
Veronica Child <vachild@me.com>
2017-01-15 22:58:22 -0600

    Post submissions and upvotes
    Able to enter posts and links

--- scripts/app.js
+++ scripts/app.js
@@ -1,40 +1,27 @@
 var app = angular.module('redditClone', []);
 app.controller('MainCtrl', function($scope) {
     // $scope bridges variable "posts" to index template
-    // defines a list of post titles
-    $scope.firstName = "John";
     $scope.test = "Hello World!";
+    // defines a list of post titles
     $scope.posts = [
         {title: 'post 1', upvotes: 5},
         {title: 'post 2', upvotes: 3},
         {title: 'post 3', upvotes: 10}
     ];
+    $scope.addPost = function() {
+        // prevents empty posts
+        if(!$scope.title || $scope.title === '') {
+            return;
+        }
+        $scope.posts.push({
+            title: $scope.title,
+            link: $scope.link,
+            upvotes: 0
+        });
+        $scope.title = '';
+        $scope.link = '';
+    };
+    $scope.incrementUpvotes = function(post) {
+        post.upvotes += 1;
+    };
 });
-
-/*function myFunction() {
-   document.getElementById("demo").innerHTML = "Paragraph changed.";
-}
-
-/*var button = document.getElementById("button");
-
-function change(){
-    document.body.style.background = "yellow";
-}
-
-button.addEventListener("click", change);
-
-var app = angular.module('redditClone', []);
-
-// controller function
-app.controller('MainCtrl', function($scope){
-        // $scope bridges variable "posts" to index template
-        // defines a list of post titles
-        $scope.test = 'Hello world!';
-        $scope.posts = [
-            {title: 'post 1', upvotes: 5},
-            {title: 'post 2', upvotes: 2},
-            {title: 'post 3', upvotes: 15},           
-            {title: 'post 4', upvotes: 9},
-            {title: 'post 5', upvotes 0}
-        ];
-});*/
