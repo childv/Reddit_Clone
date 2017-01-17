142e1a367374d603dc8d1afa1d2620004bc5d9de
Veronica Child <vachild@me.com>
2017-01-17 13:21:52 -0600

    Added CSS Styles
    Added factory to js.app
    Added Bootstrap styling from a CDM

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,38 @@
+/*Angular code for Reddit Clone frontend */
+
 var app = angular.module('redditClone', []);
-app.controller('MainCtrl', function($scope) {
+
+/* Factory:
+Most popular way to create and configure a service
+
+An object to which properties can be added. Object then returned to a controller, which can access its properties.
+
+Store data in services, objects that are wired together using DI. They organize and share code across your app
+*/
+
+// object that returns an array of posts
+app.factory('posts', [function() { 
+    var o = {
+        posts: []
+    };
+    return o;
+}])
+
+/* Controller 
+Should be thin; business logic and persistent data should be stored in a service
+
+Instantiated only when needed; discarded when not. Switch route/reload page = Angular cleans controller
+
+Do NOT store data in: cannot be easily accessed, difficult to mock, and will be lost when controller goes out of scope
+*/
+
+app.controller('MainCtrl', [
+    '$scope',
+    'posts',
+    function($scope, posts) {
     // $scope bridges variable "posts" to index template
-    $scope.test = "Hello World!";
-    // defines a list of post titles
-    $scope.posts = [
-        {title: 'post 1', upvotes: 5},
-        {title: 'post 2', upvotes: 3},
-        {title: 'post 3', upvotes: 10}
-    ];
+    // access "posts" factory services
+    $scope.posts = posts.posts;
     $scope.addPost = function() {
         // prevents empty posts
         if(!$scope.title || $scope.title === '') {
@@ -21,7 +46,15 @@ app.controller('MainCtrl', function($scope) {
         $scope.title = '';
         $scope.link = '';
     };
-    $scope.incrementUpvotes = function(post) {
+    $scope.incrementUpvotes = function(post){
         post.upvotes += 1;
     };
-});
+}]);
+
+/* Service
+When you’re using Service, it’s instantiated with the ‘new’ keyword. Because of that, you’ll add properties to ‘this’ and the service will return ‘this’. When you pass the service into your controller, those properties on ‘this’ will now be available on that controller through your service.
+*/
+
+/* Providers
+the only service you can pass into your .config() function. Use a provider when you want to provide module-wide configuration for your service object before making it available.
+*/
