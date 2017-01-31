d4065d877a29c91eb4b60fc57e6766042f8a5e25
Veronica Child <vachild@me.com>
2017-01-31 10:43:27 -0600

    Working factory
    Successful js injection into main ctrl

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,16 @@
 var app = angular.module('redditClone', []);
-app.controller('MainCtrl', function($scope) {
+app.factory('posts', [function(){
+    var o = {
+        posts: []
+    };
+    return o;
+    }]);
+
+app.controller('MainCtrl',
+    function($scope, posts) {
     // $scope bridges variable "posts" to index template
-    $scope.test = "Hello World!";
     // defines a list of post titles
-    $scope.posts = [
-        {title: 'post 1', upvotes: 5},
-        {title: 'post 2', upvotes: 3},
-        {title: 'post 3', upvotes: 10}
-    ];
+    $scope.posts = posts.posts;
     $scope.addPost = function() {
         // prevents empty posts
         if(!$scope.title || $scope.title === '') {
